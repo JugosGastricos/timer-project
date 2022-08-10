1b1201c78d3f5392bbee34ca8826f90016e475a9
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-08-10 02:34:04 -0500

    Now the timer shows miliseconds and hours are hidden until timer shows minutes > 59. Added 0s to the left when any data value is < 10. Eliminated the array in the function flag as it wasn't doing anything

--- main.js
+++ main.js
@@ -6,33 +6,55 @@ const stopButton = document.getElementById("stop");
 const printTimer = document.getElementById("timer");
 const printFlag = document.getElementById("printFlag");
 
-
-// stopButton.addEventListener("click")
-
+let miliseconds = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let count = 0;
 let interval;
-printTimer.innerText = `${hours}:${minutes}:${seconds}`;
+printTimer.innerText = `0${minutes}:0${seconds}:0${miliseconds}`;
+
+const cleanLook = (flag,param,tagId) => {
+    tagId.innerText = `${flag}${param} 0${minutes}:0${seconds}:0${miliseconds}`;
+
+    if(miliseconds > 9){
+        tagId.innerText = `${flag}${param} 0${minutes}:0${seconds}:${miliseconds}`;
+    }
+    if (seconds > 9) {
+        tagId.innerText = `${flag}${param} 0${minutes}:${seconds}:${miliseconds}`;
+    }
+    if (minutes > 9) {
+        tagId.innerText = `${flag}${param} ${minutes}:${seconds}:${miliseconds}`;
+    }
+    if (hours > 0) {
+        tagId.innerText = `${flag}${param} 0${hours}:${minutes}:${seconds}:${miliseconds}`;
+    } else if (hours > 9) {
+        tagId.innerText = `${flag}${param} ${hours}:${minutes}:${seconds}:${miliseconds}`;
+    }
+}
 
 const timer = () => {
     interval = setInterval(() => {
-        seconds ++
-        if(seconds === 5){
+        miliseconds ++
+        if(miliseconds === 100){
+            miliseconds = 0;
+            seconds ++
+        }
+        if(seconds === 60){
             seconds = 0;
             minutes ++;
-            console.log(`Minutes: ${minutes}`);
-            if (minutes === 5){
+            // console.log(`Minutes: ${minutes}`);
+            if (minutes === 60){
                 minutes = 0;
                 hours ++;
-                console.log(`Hours: ${hours}`);
+                // console.log(`Hours: ${hours}`);
             }
         }
-        console.log(`Seconds: ${seconds}`);
-        printTimer.innerText = `${hours}:${minutes}:${seconds}`;
-    }, 1000);
+        // console.log(`Seconds: ${seconds}`);
+        cleanLook("","",printTimer);
+    }, 10);
 }
+
 
 const pauseTimer = () => {
     clearInterval(interval);
@@ -42,26 +64,22 @@ const pauseTimer = () => {
 const stopTimer = () => {
     clearInterval(interval);
     interval = null;
+    miliseconds = 0;
     seconds = 0;
     minutes = 0;
     hours = 0;
     count = 0;
-    printTimer.innerText = `${hours}:${minutes}:${seconds}`;
+    printTimer.innerText = `0${minutes}:0${seconds}:0${miliseconds}`;
     printFlag.innerText = ``;
 }
 
 const flag = () => {
-    const saveFlag = [];
     count ++;
-    
-    saveFlag.push({hours, minutes, seconds});
-    saveFlag.forEach((value) => {
-        const newParagraph = document.createElement("p");
-        newParagraph.className = "new-input";
-        printFlag.appendChild(newParagraph);
-        newParagraph.innerText = `🚩${count}: ${value.hours}:${value.minutes}:${value.seconds} \n`;
-        saveFlag.shift();
-    })
+
+    const newParagraph = document.createElement("p");
+    newParagraph.className = "new-input";
+    printFlag.appendChild(newParagraph);
+    cleanLook('🚩',count + ':',newParagraph);
 }
 
 goButton.addEventListener("click", timer);
