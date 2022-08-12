86c8a0d3e70885b1e8c6723d8b9d3789c2abb599
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-08-12 02:58:52 -0500

    Improved timer logic, added time difference to flagged times and edited some styles to make the app look better

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ const stopButton = document.getElementById("stop");
 const printTimer = document.getElementById("timer");
 const printFlag = document.getElementById("printFlag");
 
+let mili = 0;
 let miliseconds = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let count = 0;
 let interval;
+let flagedTimes = [];
 
 const hideButton = (button) => {
     button.setAttribute("hidden", true);
@@ -33,22 +35,20 @@ const cleanLook = (flag,count,tagId) => {
 
 const timer = () => {
     interval = setInterval(() => {
+        mili ++
         miliseconds ++
-        if(miliseconds === 100){
+        if(mili % 100 === 0){
             miliseconds = 0;
             seconds ++
-            if(seconds === 60){
+            if(mili % 6000 === 0){
                 seconds = 0;
                 minutes ++;
-                // console.log(`Minutes: ${minutes}`);
-                if (minutes === 60){
+                if (mili % 60000 === 0){
                     minutes = 0;
                     hours ++;
-                    // console.log(`Hours: ${hours}`);
                 }
             }
         }
-        // console.log(`Seconds: ${seconds}`);
         cleanLook("","",printTimer);
     }, 10);
     
@@ -57,7 +57,6 @@ const timer = () => {
     showButton(pauseButton);
     showButton(flagButton);
 }
-
 
 const pauseTimer = () => {
     clearInterval(interval);
@@ -79,17 +78,57 @@ const stopTimer = () => {
     count = 0;
     printTimer.innerText = `0${minutes}:0${seconds}.0${miliseconds}`;
     printFlag.innerText = ``;
+    flagedTimes = [];
+    mili = 0;
     
     hideButton(stopButton);
 }
 
+
 const flag = () => {
     count ++;
+    flagedTimes.push(mili);
+    console.log(flagedTimes);
 
     const newParagraph = document.createElement("p");
+    const newDiv = document.createElement("div");
+    newDiv.className = "new-div";
     newParagraph.className = "new-input";
-    printFlag.appendChild(newParagraph);
+    newDiv.appendChild(newParagraph);
+    printFlag.appendChild(newDiv);
     cleanLook('🚩',count + ':',newParagraph);
+    
+    const newSpan = document.createElement("span");
+    newSpan.className = "new-input new-span";
+    newDiv.appendChild(newSpan);
+    
+    const timeDifference = () => {
+        let x;
+        let y;
+        let z;
+        let add;
+
+        const substract = mili - flagedTimes[flagedTimes.length - 2];
+
+        console.log(substract)
+        z = Number(Math.floor(substract)/100);
+        
+        if(flagedTimes.length > 1){
+            newSpan.innerText = `+ 00:${z}`;
+            console.log(`00:${z}`)
+        }
+
+        if (z > 60) {
+            x = Math.floor(z/10)*10;
+            y = (z - x).toFixed(2)
+            console.log(y,z);
+            add = Math.floor(x / 60);
+            console.log(`0${add}:${y}`);
+            newSpan.innerText = `+ ${add < 9 ? "0" + add : add}:${z}`;
+        }
+        
+    };
+    timeDifference();
 }
 
 goButton.addEventListener("click", timer);
